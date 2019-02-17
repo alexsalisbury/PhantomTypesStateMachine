9a9a2411532a2b0d9cbe4d49de4819b242a27cea
Alex Salisbury <alex@alexsalisbury.com>
2019-02-17 10:37:13 -0500

    Add additional info to Program.cs from investigations.

--- PhantomTypes/Program.cs
+++ PhantomTypes/Program.cs
@@ -7,9 +7,24 @@
         static void Main(string[] args)
         {
             IssueBackLog ib = new IssueBackLog();
+
+#if item_was_not_immutable
             var item = ib.Create("Test Item", "Fake issue");
-            item.Assign("Me");
-            item.Complete();
+            item.Assign("Me");// <-- You're not capturing the new state since this function returns a new object. 
+            item.Complete(); // <-- this data wouldn't have the "AssignedTo" property set
+#endif
+            // you couldn't do this, var is still static typing.
+#if this_was_javascript
+            var item = ib.Create("Test Item", "Fake issue");
+            item = item.Assign("Me");// <-- You're not getting the current object back. 
+            item = item.Complete();
+#endif
+
+            var s1 = ib.Create("Test Item", "Fake issue");
+            var s2 = s1.Assign("Me");
+            var s3 = s2.Complete(); // properly chained. I think this makes most sense on a web API.
+
+            //Assert s3.dataStore["AssignedTo"] == "Me";
 
             Console.WriteLine("Hello World!");
         }
